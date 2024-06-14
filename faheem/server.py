abdcd5ab4dadd93f8197ed7f31122a2ab6e643a1
Mamdouh Khalaf <2210002016@iau.edu.sa>
2024-06-14 20:07:50 +0300

    🐣 INIT: fastapi server

---
+++ faheem/server.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, status, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/health", status_code=status.HTTP_200_OK)
+def health():
+    return {"status": "I AM ALIVE!!"}
