d866ec1ea78e5f4f105d2341854ede1d0d459e09
Mamdouh Khalaf <2210002016@iau.edu.sa>
2024-06-18 11:42:01 +0300

    🧾 LOGS: add logging config

--- faheem/config.py
+++ faheem/config.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import logging
 
 from pathlib import Path
 
@@ -31,6 +33,46 @@ class Settings(BaseSettings):
         default=str(Path().resolve()) + str(Path("/logs").absolute()),
         description="Directory to store the logs.",
     )
+
+    LOGGING_CONFIG: dict = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "minimal": {"format": "%(message)s"},
+            "detailed": {
+                "format": "%(levelname)s %(asctime)s [%(name)s:%(filename)s:%(funcName)s:%(lineno)d]\n%(message)s\n"
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": sys.stdout,
+                "formatter": "minimal",
+                "level": logging.DEBUG,
+            },
+            "info": {
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": Path(LOGS_DIR, "info.log"),
+                "maxBytes": 10485760,  # 1 MB
+                "backupCount": 10,
+                "formatter": "detailed",
+                "level": logging.INFO,
+            },
+            "error": {
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": Path(LOGS_DIR, "error.log"),
+                "maxBytes": 10485760,  # 1 MB
+                "backupCount": 10,
+                "formatter": "detailed",
+                "level": logging.ERROR,
+            },
+        },
+        "root": {
+            "handlers": ["console", "info", "error"],
+            "level": logging.INFO,
+            "propagate": True,
+        },
+    }
 
 
 settings = Settings()
