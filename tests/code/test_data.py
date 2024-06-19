0d7fda00c6dbcea9a1b78638dee9b92cdabc65b6
Mamdouh Khalaf <2210002016@iau.edu.sa>
2024-06-20 00:44:17 +0300

    🧪 TEST: add preprocess data test

---
+++ tests/code/test_data.py
@@ -0,0 +1,15 @@
+import pytest
+
+from faheem.resources.data import preprocess_text
+
+
+@pytest.mark.parametrize(
+    "text, clean_text",
+    [
+        ("قلتُ لصاحبي، اهلًا بِك", "قلت لصاحبي اهلا بك"),
+        ("كثيـــــــــر من الناس", "كثير من الناس"),
+        ("إقنع بِمن عندك يقنع بِكَ من عنْدك؛", "اقنع بمن عندك يقنع بك من عندك"),
+    ],
+)
+def test_preprocess_text(text, clean_text):
+    assert preprocess_text(text) == clean_text
