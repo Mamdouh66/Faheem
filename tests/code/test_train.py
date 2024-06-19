f5c1226b80e63c2fc067e47e33903db2b4dd8788
Mamdouh Khalaf <2210002016@iau.edu.sa>
2024-06-20 00:26:16 +0300

    🧪 TEST: add train_model test

--- tests/code/test_train.py
+++ tests/code/test_train.py
@@ -2,7 +2,12 @@ import pytest
 
 import numpy as np
 
-from faheem.resources.train import split_data
+from faheem.resources.data import get_clean_data
+from faheem.resources.train import split_data, train_model, get_numpy_X_y
+
+from sklearn import metrics
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 
 def test_split_data():
@@ -27,7 +32,6 @@ def test_split_data():
     # Test without shuffling
     X_train_ns, X_test_ns, y_train_ns, y_test_ns = split_data(X, y, shuffle=False)
 
-    # Check that the data is not shuffled
     assert np.array_equal(X[:40], X_train_ns)
     assert np.array_equal(X[40:], X_test_ns)
     assert np.array_equal(y[:40], y_train_ns)
@@ -38,8 +42,27 @@ def test_split_data():
         X, y, test_size=0.1
     )
 
-    # Check the size of the splits
     assert X_train_small.shape[0] == 45
     assert X_test_small.shape[0] == 5
     assert y_train_small.shape[0] == 45
     assert y_test_small.shape[0] == 5
+
+
+def test_train_model():
+    clean_data = get_clean_data()
+    model = train_model(clean_data, experiment_name="Test Experiment")
+
+    assert isinstance(model, MultinomialNB)
+
+    # Extract the featuers and targets to valdiate the model
+    X, y = get_numpy_X_y(clean_data)
+    vectorizer = CountVectorizer()
+    X_vectorized = vectorizer.fit_transform(X)
+    tfidf = TfidfTransformer(use_idf=True, norm="l2", smooth_idf=True)
+    X_tfidf = tfidf.fit_transform(vectorizer.fit_transform(X)).toarray()
+
+    X_train, X_test, y_train, y_test = split_data(X_tfidf, y)
+    y_pred = model.predict(X_test)
+
+    accuracy = metrics.accuracy_score(y_test, y_pred)
+    assert accuracy > 0.7
