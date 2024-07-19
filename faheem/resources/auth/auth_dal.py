afe1b7a8c5600c7393be6bee25d8e9f85206a681
Mamdouh Khalaf <2210002016@iau.edu.sa>
2024-07-20 00:01:32 +0300

    🦺 VALIDATION: add validation to creating users

--- faheem/resources/auth/auth_dal.py
+++ faheem/resources/auth/auth_dal.py
@@ -2,22 +2,34 @@ import uuid
 import sqlalchemy as sa
 
 from faheem.resources.auth import auth_schemas, auth_models
+from faheem.config import logger
 
 from sqlalchemy.orm import Session
+from pydantic_core import ValidationError
 
 
-def create_new_user(user: auth_schemas.UserCreate, db: Session = None):
-    new_user = auth_models.User(
-        id=uuid.uuid4(),
-        username=user.username,
-        password=user.password,
-        company_id=user.company_id,
-        role=user.role,
-        last_login=sa.func.now(),
-        created_at=sa.func.now(),
-        is_active=True,
-    )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+def create_new_user(
+    user: auth_schemas.UserCreate, db: Session = None
+) -> auth_models.User | None:
+    try:
+        new_user = auth_models.User(
+            id=uuid.uuid4(),
+            username=user.username,
+            password=user.password,
+            company_id=user.company_id,
+            role=user.role,
+            last_login=sa.func.now(),
+            created_at=sa.func.now(),
+            is_active=True,
+        )
+
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+    except ValidationError as v_e:
+        logger.error(f"Failed to validate model: {v_e}")
+        return None
+    except Exception as e:
+        logger.error(f"Failed to create new user: {e}")
+        return None
     return new_user
--- faheem/resources/auth/auth_schemas.py
+++ faheem/resources/auth/auth_schemas.py
@@ -4,7 +4,7 @@ from uuid import UUID
 
 from typing import Optional
 from datetime import datetime
-from pydantic import BaseModel, SecretStr, field_validator
+from pydantic import BaseModel, SecretStr, field_validator, ConfigDict
 
 
 def validate_username(username: str) -> str:
@@ -15,6 +15,8 @@ def validate_username(username: str) -> str:
 
 
 class UserBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: UUID
     company_id: UUID
     username: str
